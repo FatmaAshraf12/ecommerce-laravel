0d3272df72f0d7ee20738afccf8351f4c565e60c
Fatma Ashraf <fatma_ashraf12@yahoo.com>
2022-12-18 17:13:58 +0200

    Product Sorting and Products Per Page

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -8,9 +8,30 @@ use App\Models\Product;
 class ShopComponent extends Component
 {
     use WithPagination;
+    public $pageSize = 12;
+    public $orderBy = 'Default Sorting';
+
+    public function changePageSize($size){
+        $this->pageSize = $size;
+    }
+
+    public function changeOrderBy($orderBy){
+        $this->orderBy = $orderBy;
+    }
+
     public function render()
     {
-        $products = Product::paginate(12);
+        if($this->orderBy == 'Price: Low to High')
+        $products = Product::orderBy('regular_price','ASC')->paginate($this->pageSize);
+
+        else if($this->orderBy == 'Price: High to Low')
+        $products = Product::orderBy('regular_price','DESC')->paginate($this->pageSize);
+
+        else if($this->orderBy == 'By newness')
+        $products = Product::orderBy('created_at','DESC')->paginate($this->pageSize);
+
+        else
+        $products = Product::paginate($this->pageSize);
         return view('livewire.shop-component' , ['products'=>$products]);
     }
 }
