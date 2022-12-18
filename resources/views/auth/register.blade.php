e50b9d277fa703d68489548944f2968b9e79fe11
Fatma Ashraf <fatma_ashraf12@yahoo.com>
2022-12-18 15:15:31 +0200

    Changing the Login and Register Page Layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{--  <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +49,73 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<x-app-layout>
+    <main class="main">
+        <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="/" rel="nofollow">Home</a>
+                    <span></span> Register
+                </div>
+            </div>
+        </div>
+        <section class="pt-150 pb-150">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10 m-auto">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="login_wrap widget-taber-content p-30 background-white border-radius-5">
+                                    <div class="padding_eight_all bg-white">
+                                        <div class="heading_s1">
+                                            <h3 class="mb-30">Create an Account</h3>
+                                        </div>
+                                        <form method="post" action="{{ route('register') }}">
+                                            @csrf
+                                            <div class="form-group">
+                                                <input type="text" required name="name" placeholder="Name"
+                                                    :value="old('name')" autofocus>
+                                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="text" required name="email" placeholder="Email"
+                                                    :value="old('email')" autofocus>
+                                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+                                            </div>
+                                            <div class="form-group">
+                                                <input required type="password" name="password" placeholder="Password"
+                                                    autofocus>
+                                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
+                                            </div>
+                                            <div class="form-group">
+                                                <input required type="password" name="password_confirmation"
+                                                    id="password_confirmation" placeholder="Confirm password" autofocus>
+                                                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+
+                                            </div>
+
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-fill-out btn-block hover-up"
+                                                    name="login">Submit &amp; Register</button>
+                                            </div>
+                                        </form>
+                                        <div class="text-muted text-center">Already have an account? <a
+                                                href="{{ route('login') }}">Sign in now</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <img src="{{ asset('assets/imgs/login.png') }}">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</x-app-layout>
